cd233f39d7fd914c7fa1d11cb54603811ab2de53
TheScriptRailoth <am3718440@gmail.com>
2023-07-17 00:43:42 +0530

    Added Elevated Button & Appbar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -21,6 +21,101 @@ class _HomeScreenState extends State<HomeScreen> {
             IconButton(onPressed: (){}, icon: Icon(Icons.shopping_bag_outlined),color: Colors.black,),
           ],
         ),
+      body: Column(
+        children:[ Row(
+        children: [
+          Expanded(
+            child: Container(
+              height: 43,
+              margin: EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Button 1 functionality
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  onPrimary: Colors.black,
+                  side:BorderSide( width: 2, color: Colors.black),
+                  elevation: 0.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[
+                    Container(
+                      width: 25, // Set the desired width
+                      height: 30, // Set the desired height
+                      child: Image.asset(
+                        'assets/fasion_image.png', // Replace with your image path
+                        fit: BoxFit.cover, // Adjust the fit property as needed
+                      ),
+                    ),
+                    SizedBox(width: 10,),
+                    Text('Fasion', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  ]
+                )
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 43,
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                  onPressed: () {
+                    // Button 1 functionality
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    onPrimary: Colors.black,
+                    side:BorderSide( width: 2, color: Colors.black),
+                    elevation: 0.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                  ),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children:[
+                        Container(
+                          width: 25, // Set the desired width
+                          height: 30, // Set the desired height
+                          child: Image.asset(
+                            'assets/beauty_image.png', // Replace with your image path
+                            fit: BoxFit.cover, // Adjust the fit property as needed
+                          ),
+                        ),
+                        SizedBox(width: 10,),
+                        Text('Beauty', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      ]
+                  )
+              ),
+            ),
+          ),
+        ],
+      ),
+      SizedBox(height: 10),
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 28,
+                  backgroundImage: AssetImage('assets/myntra_logo.png'),
+                ),
+                SizedBox(height: 10,),
+                Text('CATEGORIES')
+              ],
+            ),
+            SizedBox(width: 20,)
+          ],
+        ),
+            )]
+      ),
     );
   }
 }
