17ccaa051c4efff3c9c147c39710249bc0f06f32
TheScriptRailoth <am3718440@gmail.com>
2023-07-18 20:55:18 +0530

    Added App Bar in Inside Page

--- lib/insider_page.dart
+++ lib/insider_page.dart
@@ -9,6 +9,28 @@ class InsiderPage extends StatefulWidget {
 class _InsiderPageState extends State<InsiderPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        title: const Text('MYNTRA INSIDER', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87, fontSize: 16),),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 250,
+              decoration: BoxDecoration(
+                color: Colors.black45,
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))
+              ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myntra_clone/home_screen.dart';
+import 'package:myntra_clone/insider_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Myntra Clone',
       theme: ThemeData(),
-      home: HomeScreen(),
+      home: InsiderPage(),
       debugShowCheckedModeBanner: false,
     );
   }
