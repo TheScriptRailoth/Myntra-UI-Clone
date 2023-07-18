a38f45435003d09af0858965edaa2c467484ee09
TheScriptRailoth <am3718440@gmail.com>
2023-07-19 00:15:08 +0530

    Added Design Inside Page

--- lib/insider_page.dart
+++ lib/insider_page.dart
@@ -7,6 +7,8 @@ class InsiderPage extends StatefulWidget {
 }
 
 class _InsiderPageState extends State<InsiderPage> {
+  Color GoldenColor = Color(int.parse('0xFFAE8847'));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,15 +24,79 @@ class _InsiderPageState extends State<InsiderPage> {
           children: [
             Container(
               width: double.infinity,
-              height: 250,
+              height: 438,
               decoration: BoxDecoration(
-                color: Colors.black45,
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))
+                color: GoldenColor,
+                borderRadius: BorderRadius.only(bottomRight: Radius.circular(20), bottomLeft: Radius.circular(20))
               ),
-            ),
+              child: Column(
+                children: [
+                  Stack(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 375,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30))
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 375,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage('https://assets.myntassets.com/assets/images/retaillabs/2023/4/24/ec57d5ef-5c23-4e2b-92b8-a41f7a1f3ede1682332607762-Frame-18247.png'), // Replace with your photo image path
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 15, top: 20),
+                        child: Icon(Icons.info_outline_rounded, color: Colors.white, size: 20),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 18, left: 6),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Shop for \u20B92000 before 13th Oct to retain your Select benefits.',
+                                style: TextStyle(color: Colors.white, fontSize: 13),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 6),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Shop Now',
+                                style: TextStyle(color: Colors.white, fontSize: 13),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  )
+                ]
+              ),
+            )
           ],
         ),
       )
     );
+  }
+  Color hexToColor(String hexCode) {
+    final hexDigits = hexCode.replaceAll('#', '');
+    final hexValue = int.parse(hexDigits, radix: 16);
+    return Color(hexValue);
   }
 }
